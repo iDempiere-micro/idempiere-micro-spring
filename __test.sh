a0e9bfaf44cce36383221a7e6f8b8807bbd1e909
davidpodhola <david.podhola@hsharp.software>
2018-09-17 08:25:35 +0200

    try dockerize on travis

--- __test.sh
+++ __test.sh
@@ -4,7 +4,7 @@ set -o pipefail
 # make run will also run the tests
 make run &
 # so we will allow it to take it's time
-sleep 140
+dockerize -wait http://localhost:8080 -timeout 3m
 cd integration_tests
 npm i
 ./run.sh
