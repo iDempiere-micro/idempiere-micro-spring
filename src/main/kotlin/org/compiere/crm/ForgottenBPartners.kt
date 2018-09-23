f5e31dc4e17deeb1cde2252ca8ce31d80cca65ba
davidpodhola <david@podhola.net>
2018-09-23 15:44:56 +0200

    refactoring

--- src/main/kotlin/org/compiere/crm/ForgottenBPartners.kt
+++ src/main/kotlin/org/compiere/crm/ForgottenBPartners.kt
@@ -1,14 +1,7 @@
 package org.compiere.crm
 
 class ForgottenBPartners : SimpleBPartnerSearch() {
-    override fun getSql(): String {
-        val columns =
-            if (full) { "*, C_ContactActivity_ID as activity_C_ContactActivity_ID" } else { "c_bpartner_id,name,taxid" }
-
-        return """select $columns from adempiere.bpartner_v
-        where ( activity_startdate < current_date or c_contactactivity_id is null )
-        and ad_client_id IN (0, ?) and ( ad_org_id IN (0,?) or ? = 0) and isactive = 'Y' -- params 1..3
-        order by name asc
-        """
+    override fun whereCondition(): String {
+        return "activity_startdate < current_date or c_contactactivity_id is null"
     }
 }
