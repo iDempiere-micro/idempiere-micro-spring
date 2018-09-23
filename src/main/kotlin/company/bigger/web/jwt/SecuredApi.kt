394c5a26c206556cc36e42378f1769ef275d56de
davidpodhola <david@podhola.net>
2018-09-23 12:16:48 +0200

    code comments cleanup

--- src/main/kotlin/company/bigger/web/jwt/SecuredApi.kt
+++ src/main/kotlin/company/bigger/web/jwt/SecuredApi.kt
@@ -7,20 +7,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
-import javax.servlet.http.HttpServletResponse
 
 /**
- * Aspect whose goal is to check automatically that methods
- * having a @ApiKeySecured annotation are correctly accessed
- * by users having a valid API Key (JWT).
- * A check against the user service is done to find the user
- * having the api key passed as request header/parameter.
- * If the API Key is valid the annotated method is executed,
- * otherwise the response is set with an UNAUTHORIZED status and the annotated
- * method is not executed.
+ * Component to test if the authorization token is valid
  */
 @Component
-class SecuredApi() {
+class SecuredApi {
     @Autowired
     lateinit var userService: UserService
 
@@ -28,6 +20,9 @@ class SecuredApi() {
         private val LOG = LoggerFactory.getLogger(SecuredApi::class.java)
     }
 
+    /**
+     * If the [authorization] is JWT-valid, process [execute], otherwise return HttpStatus.UNAUTHORIZED
+     */
     fun <T> processAuthorization(authorization: String?, execute: () -> T): ResponseEntity<T?> {
         val apiKey = authorization?.replace("Token ", "")?.replace("Bearer ", "")
         val failedResult = null as T?
@@ -43,7 +38,7 @@ class SecuredApi() {
         val user = userService.findByToken(apiKey ?: "")
         LOG.info("user by token: ${user?.loginName}")
         if (user == null) {
-            LOG.info("No user with Authorization: {}, returning {}.", apiKey, HttpServletResponse.SC_UNAUTHORIZED)
+            LOG.info("No user with Authorization: {}, returning {}.", apiKey, HttpStatus.UNAUTHORIZED)
 
             return ResponseEntity(failedResult, HttpStatus.UNAUTHORIZED)
         } else {
@@ -52,7 +47,7 @@ class SecuredApi() {
                 LOG.info("Validating JWT")
                 if (!userService.validToken(apiKey ?: "", user)) {
                     LOG.info("JWT invalid")
-                    LOG.info("Authorization: {} is an invalid JWT.", apiKey, HttpServletResponse.SC_UNAUTHORIZED)
+                    LOG.info("Authorization: {} is an invalid JWT.", apiKey, HttpStatus.UNAUTHORIZED)
 
                     return ResponseEntity(failedResult, HttpStatus.UNAUTHORIZED)
                 }
