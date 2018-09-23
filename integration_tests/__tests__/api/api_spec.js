430b6bf2c180b998df825a0ed3f8380e8d3a90e2
davidpodhola <david.podhola@hsharp.software>
2018-09-23 10:38:46 +0200

    [wip] integration test for token too

--- integration_tests/__tests__/api/api_spec.js
+++ integration_tests/__tests__/api/api_spec.js
@@ -1,7 +1,7 @@
 const frisby = require('frisby');
 const Joi = frisby.Joi; // Frisby exports Joi for convenience on type assersions
 
-it ('iDempiere login should work ', function () {
+it ('iDempiere login should work', function () {
     return frisby
       .setup({
         request: {
@@ -13,4 +13,32 @@ it ('iDempiere login should work ', function () {
       .get('http://localhost:8080/user/GardenUser/login/GardenUser')
       .expect('status', 200)
       .expect('json', 'logged', true);
+});
+
+it ('GardenUser can login and token works', function () {
+    return frisby
+      .setup({
+        request: {
+          headers: {
+            'Content-Type': 'text/plain; charset=UTF-8',
+          }
+        }
+      })
+      .get('http://localhost:8080/user/GardenUser/login/GardenUser')
+      .expect('status', 200)
+      .expect('json', 'logged', true)      
+      .then(function (res) { // res = FrisbyResponse object
+        let token = res.json[0].token;
+	return frisby
+	  .setup({
+        request: {
+          headers: {
+            'Content-Type': 'text/plain; charset=UTF-8',
+	    'Authorization': 'Token ' + token 
+          }
+        }
+      })
+      .get('http://localhost:8080/user/me')
+      .expect('status', 200);
+      });	      
 });
