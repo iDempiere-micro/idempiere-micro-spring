df631b5a69f61409a75383b091d58539d7dff8f0
davidpodhola <david@podhola.net>
2018-10-04 19:57:28 +0200

    accounting test WIP

--- src/test/kotlin/org/compiere/invoicing/test/InvoiceTests.kt
+++ src/test/kotlin/org/compiere/invoicing/test/InvoiceTests.kt
@@ -1,11 +1,19 @@
 package org.compiere.invoicing.test
 
 import company.bigger.test.support.BaseComponentTest
+import org.compiere.accounting.MOrder
+import org.compiere.accounting.MOrderLine
+import org.compiere.accounting.MProduct
+import org.compiere.accounting.MWarehouse
+import org.compiere.crm.MBPartner
 import org.compiere.invoicing.MInvoice
 import org.compiere.model.I_C_Invoice
+import org.compiere.model.I_C_Order
 import org.compiere.orm.DefaultModelFactory
 import org.compiere.orm.IModelFactory
+import org.idempiere.common.util.Env
 import org.junit.Test
+import java.sql.Timestamp
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
@@ -25,4 +33,26 @@ class InvoiceTests : BaseComponentTest() {
         assertNotNull(lines)
         assertEquals(6, lines.count())
     }
+
+    @Test
+    fun `create invoice from order`() {
+        val order = MOrder(Env.getCtx(), 0, null)
+        order.aD_Org_ID = 50001
+        order.m_Warehouse_ID = 50002
+
+        val id = 118
+        val partner = MBPartner.get(Env.getCtx(), id)
+
+        val product_id = 136
+        val product = MProduct.get(Env.getCtx(), product_id)
+
+        order.setBPartner(partner)
+        order.save()
+
+        val orderLine = MOrderLine(order)
+        orderLine.product = product
+        orderLine.setQty(1.toBigDecimal())
+        orderLine.save()
+    }
+
 }
