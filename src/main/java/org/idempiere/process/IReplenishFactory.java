b816d25b0f36ffeb9f1ba69a3481f52b5e95fae2
davidpodhola <david@podhola.net>
2018-10-23 23:54:04 +0200

    processes

---
+++ src/main/java/org/idempiere/process/IReplenishFactory.java
@@ -0,0 +1,13 @@
+package org.idempiere.process;
+
+import org.compiere.util.ReplenishInterface;
+
+public interface IReplenishFactory {
+
+    /**
+     *
+     * @param className
+     * @return Replenish instance
+     */
+    public ReplenishInterface newReplenishInstance(String className);
+}
