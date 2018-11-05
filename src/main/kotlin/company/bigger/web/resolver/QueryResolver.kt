dad49a68a7321e84fd7e7dede4fb651857ab5b0b
David Podhola <david@podhola.net>
2018-11-05 21:39:02 +0100

    countries etc.

--- src/main/kotlin/company/bigger/web/resolver/QueryResolver.kt
+++ src/main/kotlin/company/bigger/web/resolver/QueryResolver.kt
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Component
 class QueryResolver : BaseResolver() {
     val version = "1.0.0"
     val me get() = userService?.currentUser()
+    val users get() = userService?.getUsers()
+    val categories get() = categoryService?.getAllCategories()
+    val countries get() = countryService?.getAllCountries()
 }
