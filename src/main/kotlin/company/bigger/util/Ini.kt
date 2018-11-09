1b86d5afda30499a196eadd3c3b643a05d9fe903
davidpodhola <david.podhola@hsharp.software>
2018-11-09 07:36:52 +0100

    more code comments

--- src/main/kotlin/company/bigger/util/Ini.kt
+++ src/main/kotlin/company/bigger/util/Ini.kt
@@ -3,6 +3,12 @@ package company.bigger.util
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
+/**
+ * The original Ini class from iDempiere to collect the configuration parameters.
+ * It is still used for the connection string etc.
+ * Note some local connection variables (like `jwt.issuer`) if used once-only
+ * are declared in the place where needed.
+ */
 @Configuration
 open class Ini() {
     @Value("\${connection}")
